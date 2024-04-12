0cfbe1e6f7a5e869bd809b673884756ac129667c
ahmadxgani <ahmadxgani01@gmail.com>
2024-04-12 19:27:10 +0700

    fix: error mixed content when access via ngrok

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ config('app.name') }}</title>
     @vite(['resources/sass/app.scss', 'resources/sass/themes/dark/app-dark.scss', 'resources/leaflet/leaflet.css'])
     <link rel="shortcut icon" href="{{ asset('assets/static/images/logo/favicon.svg') }}" type="image/x-icon">
