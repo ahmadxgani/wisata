434d748cdf8b56e0bde9277539fa566b304e955b
ahmadxgani <ahmadxgani01@gmail.com>
2024-04-13 16:21:43 +0700

    wip: init destination resource

---
+++ app/Http/Controllers/DestinationController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Destination;
+use Illuminate\Http\Request;
+
+class DestinationController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Destination $destination)
+    {
+        return view('detail');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Destination $destination)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Destination $destination)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Destination $destination)
+    {
+        //
+    }
+}
