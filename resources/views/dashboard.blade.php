d3e87770a50c97fe4d88cd9b082c892735edbe8c
ahmadxgani <ahmadxgani01@gmail.com>
2024-04-13 17:05:18 +0700

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,8 +6,8 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Layout Default</h3>
-                    <p class="text-subtitle text-muted">The default layout.</p>
+                    <h3>Manage Destination</h3>
+                    <p class="text-subtitle text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
                     <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
@@ -21,17 +21,70 @@
         </div>
         <section class="section">
             <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">Default Layout</h4>
-                </div>
-                <div class="card-body">
-<p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, commodi? Ullam quaerat similique iusto
-                    temporibus, vero aliquam praesentium, odit deserunt eaque nihil saepe hic deleniti? Placeat delectus
-                    quibusdam ratione ullam!
+                <div class="card-content">
+                    <div class="table-responsive">
+                        <table class="table mb-0 table-lg">
+                            <thead>
+                                <tr>
+                                    <th>NAME</th>
+                                    <th>ADDRESS</th>
+                                    <th>ACTION</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="text-bold-500">Michael Right</td>
+                                    <td>Sukabumi</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-primary">Detail</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Delete</a>
+                                    </td>
 
-</p>
-<a href="{{ route('logout') }}" class="btn btn-primary">Logout</a>
+                                </tr>
+                                <tr>
+                                    <td class="text-bold-500">Morgan Vanblum</td>
+                                    <td>Sukabumi</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-primary">Detail</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Delete</a>
+                                    </td>
+
+                                </tr>
+                                <tr>
+                                    <td class="text-bold-500">Tiffani Blogz</td>
+                                    <td>Sukabumi</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-primary">Detail</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Delete</a>
+                                    </td>
+
+                                </tr>
+                                <tr>
+                                    <td class="text-bold-500">Ashley Boul</td>
+                                    <td>Sukabumi</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-primary">Detail</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Delete</a>
+                                    </td>
+
+                                </tr>
+                                <tr>
+                                    <td class="text-bold-500">Mikkey Mice</td>
+                                    <td>Sukabumi</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-primary">Detail</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                        <a href="#" class="btn btn-sm btn-primary">Delete</a>
+                                    </td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </section>
