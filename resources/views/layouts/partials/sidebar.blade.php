671426a0cb334944e04435b97d4641dd47afdae1
ahmadxgani <ahmadxgani01@gmail.com>
2024-04-13 16:41:50 +0700

    update sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,6 +1,7 @@
 <x-mazer-sidebar :href="route('dashboard')" :logo="asset('assets/static/images/logo/logo.png')">
-    <x-mazer-sidebar-item icon="bi bi-grid-fill" :link="route('dashboard')" name="Dashboard" />
-    <x-mazer-sidebar-item icon="bi bi-stack" :link="route('dashboard')" name="Components">
-        <x-mazer-sidebar-subitem :link="route('component.accordion')" name="Accordion" :active="false"/>
+    <x-mazer-sidebar-item icon="bi bi-grid-fill" :link="route('dashboard')" name="Manage Destination" />
+    <x-mazer-sidebar-item icon="bi bi-stack" :link="route('dashboard')" name="Account">
+        <x-mazer-sidebar-subitem :link="route('component.accordion')" name="Profile" :active="false"/>
+        <x-mazer-sidebar-subitem :link="route('logout')" name="Logout" :active="false"/>
     </x-mazer-sidebar-item>
 </x-mazer-sidebar>
