26cee7a235e291f2655ec043fc4cc661bb2bd60c
ahmadxgani <ahmadxgani01@gmail.com>
2024-04-26 05:22:35 +0700

    update geolocation coordinate on leaflet

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,5 +1,5 @@
-const map = L.map('map').setView([36.00000000, 138.00000000], 10);
-// const map = L.map('map').setView([-6.91806000, 106.92667000], 13);
+// const map = L.map('map').setView([36.00000000, 138.00000000], 10);
+const map = L.map('map').setView([-6.91806000, 106.92667000], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
