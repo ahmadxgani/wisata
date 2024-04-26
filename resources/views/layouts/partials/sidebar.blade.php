386e4b5dfe7363560284bc857542ea60a5b10adf
ahmadxgani <ahmadxgani01@gmail.com>
2024-04-26 10:59:49 +0700

    update sidebar, move the switch theme button to profile page

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<x-mazer-sidebar :href="route('dashboard')" :logo="asset('assets/static/images/logo/logo.png')">
+<x-mazer-sidebar>
     <x-mazer-sidebar-item icon="bi bi-grid-fill" :link="route('dashboard')" name="Destinasi" />
     <x-mazer-sidebar-item icon="bi bi-database-fill" :link="route('kategori.index')" name="Kategori"/>
     <x-mazer-sidebar-item icon="bi bi-person-circle" :link="route('dashboard')" name="Akun">
